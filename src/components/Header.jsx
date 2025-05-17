2c7782f383aa2b1dc6187b09f7278390e4115e98
Oskar <httpllkh@gmail.com>
2025-05-17 19:02:09 +0500

    add header test o

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header style={{ padding: '1rem', background: '#282c34', color: '#fff' }}>
+            <h1>Alpha App</h1>
+        </header>
+    );
+};
+
+export default Header;
